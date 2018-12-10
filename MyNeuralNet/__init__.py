8d8fe2e81de1f26e6c207277d7a71a167a5d805c
Jacob Cooper <jcooper036@gmail.com>
2018-12-09 19:33:44 -0700

    Deleted some errant files

--- MyNeuralNet/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""init for my custom neural net"""
-
-# classes
-from MyNeuralNet.layer import Layer
-from MyNeuralNet.FClayer import FClayer
-from MyNeuralNet.ActivationLayer import ActivationLayer
-from MyNeuralNet.network import Network
-from MyNeuralNet.ConvLayer import ConvLayer
